161142a4af3b16b569a8acb09a1f6ea7046d91ef
lambdalice <marisachar@gmail.com>
2013-06-22 14:00:34 +0900

    Replace tabs to 4 spaces

--- OSC/OSECPU.cs
+++ OSC/OSECPU.cs
@@ -4,58 +4,58 @@ using System;
 
 namespace OSC
 {
-	public enum OSECode1
-	{
-	    NoOp = 0x00
-	}
-	
-	public enum OSECode2
-	{
-	    Cond = 0x04
-	}
-	
-	public enum OSECode3
-	{
-	    Copy = 0x10,
-	    PCopy = 0x1E
-	}
-	
-	public enum OSECode4
-	{
-	    Or = 0x10,
-	    Xor = 0x11,
-	    And = 0x12,
-	    Add = 0x14,
-	    Sub = 0x15,
-	    Multi = 0x16,
-	    ShiftL = 0x18,
-	    ShiftR = 0x19,
-	    CompEq = 0x20,
-	    CompNeq = 0x21,
-	    CompLess = 0x22,
-	    CompGreEq = 0x23,
-	    CompLesEq = 0x24,
-	    CompGret = 0x25,
-	    PCompEq = 0x28,
-	    PCompNeq = 0x29,
-	    PCompLess = 0x2A,
-	    PCompGreEq = 0x2B,
-	    PCompLesEq = 0x2C,
-	    PCompGret = 0x2D
-	}
-	
-	public enum OSECode6
-	{
-	    Label = 0x01,
-	    LoadImm = 0x02,
-	    PLoadImm = 0x03
-	}
-	
-	public enum OSECode8
-	{
-		LoadMem = 0x08,
-		StoreMem = 0x09,
-		PAdd = 0x0E,
-		PDiff = 0x0F
-	}
+    public enum OSECode1
+    {
+        NoOp = 0x00
+    }
+    
+    public enum OSECode2
+    {
+        Cond = 0x04
+    }
+    
+    public enum OSECode3
+    {
+        Copy = 0x10,
+        PCopy = 0x1E
+    }
+    
+    public enum OSECode4
+    {
+        Or = 0x10,
+        Xor = 0x11,
+        And = 0x12,
+        Add = 0x14,
+        Sub = 0x15,
+        Multi = 0x16,
+        ShiftL = 0x18,
+        ShiftR = 0x19,
+        CompEq = 0x20,
+        CompNeq = 0x21,
+        CompLess = 0x22,
+        CompGreEq = 0x23,
+        CompLesEq = 0x24,
+        CompGret = 0x25,
+        PCompEq = 0x28,
+        PCompNeq = 0x29,
+        PCompLess = 0x2A,
+        PCompGreEq = 0x2B,
+        PCompLesEq = 0x2C,
+        PCompGret = 0x2D
+    }
+    
+    public enum OSECode6
+    {
+        Label = 0x01,
+        LoadImm = 0x02,
+        PLoadImm = 0x03
+    }
+    
+    public enum OSECode8
+    {
+        LoadMem = 0x08,
+        StoreMem = 0x09,
+        PAdd = 0x0E,
+        PDiff = 0x0F
+    }
 }
--- OSC/Program.cs
+++ OSC/Program.cs
@@ -6,28 +6,28 @@ using System.IO;
 
 namespace OSC
 {
-	class Test
-	{
-		public static void Main()
-		{
-			var sb = new StringBuilder();
-			using(var str = File.Open("test.ose", FileMode.OpenOrCreate, FileAccess.Write))
-	        using(var ose = new OSEGenerator(str))
-			{
-				ose.Emit(OSECode6.LoadImm, 0x00, 0x12345678);
-				ose.Emit(OSECode6.LoadImm, 0x01, 0x24682468);
-				ose.Emit(OSECode4.CompLess, 0x02, 0x00, 0x01);
-			}
-			
-			using(var str = File.OpenRead("test.ose"))
-			using(var rdr = new BinaryReader(str))
-			{
-				foreach(var b in rdr.ReadBytes((int)str.Length))
-					Console.Write(b.ToString("X2") + " ");
+    class Test
+    {
+        public static void Main()
+        {
+            var sb = new StringBuilder();
+            using(var str = File.Open("test.ose", FileMode.OpenOrCreate, FileAccess.Write))
+            using(var ose = new OSEGenerator(str))
+            {
+                ose.Emit(OSECode6.LoadImm, 0x00, 0x12345678);
+                ose.Emit(OSECode6.LoadImm, 0x01, 0x24682468);
+                ose.Emit(OSECode4.CompLess, 0x02, 0x00, 0x01);
+            }
+            
+            using(var str = File.OpenRead("test.ose"))
+            using(var rdr = new BinaryReader(str))
+            {
+                foreach(var b in rdr.ReadBytes((int)str.Length))
+                    Console.Write(b.ToString("X2") + " ");
                 // 05 E1 00 02 00 12 34 56 78 02 01 24 68 24 68 22 02 00 01
-			}
-			
-			Console.Read();
-		}
-	}
+            }
+            
+            Console.Read();
+        }
+    }
 }
